d0c23d99a60bf331261969b18bb74a040b0a1f3c
katlanagesh <nageshkatla99@gmail.com>
2024-03-03 11:43:23 +0530

    context

---
+++ src/Components/StudentsDetails.js
@@ -0,0 +1,27 @@
+const StudentDetails = [
+    {   
+        id    :1,
+        name  :"Nagesh",
+        age   :24,
+        gender:"male",
+        class :"Btech",
+        who   :'student'
+    },
+    {   
+        id    :2,
+        name  :"Ravali",
+        age   :25,
+        gender:"Female",
+        class :"PG",
+        who   :'student'
+    },
+    {   
+        id    :3,
+        name  :"Lucky",
+        age   :22,
+        gender:"Female",
+        class :"Degree",
+        who   :'student'
+    },
+    ]
+    export default StudentDetails ;
